10bdb05ed06b14b637ff3386ce82dfe0620726e9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-25 11:01:12 +1200

    YESSSSs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 var mouse_inside = false
 var click = false
-var distance = Vector2(0,0)
+var velocity = Vector2(0,0)
 var jump_distance = Vector2(0,0)
 var position1 = Vector2(0,0)
 var position2 = Vector2(0,0)
@@ -15,8 +15,6 @@ func _ready():
 func _process(_delta):
 	pass
 
-
-
 func _on_mouse_entered():
 	mouse_inside = true
 
@@ -24,14 +22,12 @@ func _on_mouse_exited():
 	mouse_inside = false
 
 func _input(event):
-	if event is InputEventMouseButton and event.get_button_index ( ) == 1:
+	if event is InputEventMouseButton and event.get_button_index ( ) == 1 and is_equal_approx(0, self.linear_velocity.x):
 		if event.is_pressed() and mouse_inside == true:
 			click = true
 			position1 = get_viewport().get_mouse_position()
 		if event.is_released() and click == true:
 			click = false
 			position2 = get_viewport().get_mouse_position()
-			distance = position1 - position2
-			jump_distance = -distance
-			print(jump_distance)
-			
+			velocity = position1 - position2
+			apply_impulse(velocity*10, Vector2.ZERO)
