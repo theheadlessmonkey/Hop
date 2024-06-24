0afab8be0f72f41c3f448c7fc3480f6af69e48ff
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-24 13:26:24 +1200

    done

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
-var jump_distance = 0
 var mouse_inside = false
+var click = false
+var distance = Vector2(0,0)
+var jump_distance = Vector2(0,0)
+var position1 = Vector2(0,0)
+var position2 = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,9 +24,14 @@ func _on_mouse_exited():
 	mouse_inside = false
 
 func _input(event):
-	if event is InputEventMouseButton and event.get_button_index ( ) == 1 and mouse_inside == true:
-		if event.is_pressed():
-			print("click")
-	elif event.is_pressed() and event.get_button_index ( ) == 1 and mouse_inside == false:
-		print("release")
-		
+	if event is InputEventMouseButton and event.get_button_index ( ) == 1:
+		if event.is_pressed() and mouse_inside == true:
+			click = true
+			position1 = get_viewport().get_mouse_position()
+		if event.is_released() and click == true:
+			click = false
+			position2 = get_viewport().get_mouse_position()
+			distance = position1 - position2
+			jump_distance = -distance
+			print(jump_distance)
+			
