39d04a68b9d2b5c798462f0df45e835cbf9b0633
BURNSIDE HIGH SCHOOL <22462@burnside.school.nz>
2024-06-26 14:16:29 +1200

    done jumping mechanic

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,8 @@ var velocity = Vector2(0,0)
 var jump_distance = Vector2(0,0)
 var position1 = Vector2(0,0)
 var position2 = Vector2(0,0)
+var x_clamp_value = 900
+var y_clamp_value = 1500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,12 +24,14 @@ func _on_mouse_exited():
 	mouse_inside = false
 
 func _input(event):
-	if event is InputEventMouseButton and event.get_button_index ( ) == 1 and is_equal_approx(0, self.linear_velocity.x):
+	if event is InputEventMouseButton and event.get_button_index ( ) == 1 and is_equal_approx(0, self.linear_velocity.x) and is_equal_approx(0, self.linear_velocity.y):
 		if event.is_pressed() and mouse_inside == true:
 			click = true
 			position1 = get_viewport().get_mouse_position()
 		if event.is_released() and click == true:
 			click = false
 			position2 = get_viewport().get_mouse_position()
-			velocity = position1 - position2
-			apply_impulse(velocity*10, Vector2.ZERO)
+			velocity = (position1 - position2)*5
+			velocity = Vector2(clamp(velocity.x, -x_clamp_value, x_clamp_value),clamp(velocity.y, -y_clamp_value,y_clamp_value))
+			apply_impulse(velocity, Vector2.ZERO)
+
